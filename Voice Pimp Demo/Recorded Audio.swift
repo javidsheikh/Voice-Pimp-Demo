19e0b644c97278cc3921bfbe5a268c87f5c17e48
javidsheikh <javid.sheikh@me.com>
2016-02-19 15:50:51 +0000

    Add NSCoding methods

--- Voice Pimp Demo/Recorded Audio.swift
+++ Voice Pimp Demo/Recorded Audio.swift
@@ -8,12 +8,25 @@
 
 import Foundation
 
-class RecordedAudio: NSObject {
+class RecordedAudio: NSObject, NSCoding {
+    
     var filePathURL: NSURL
     var title: String
-    
+
     init(filePathURL: NSURL, title: String) {
         self.filePathURL = NSURL(fileURLWithPath: "")
         self.title = title
+    }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(archiver: NSCoder) {
+        archiver.encodeObject(filePathURL, forKey: "filePathURL")
+        archiver.encodeObject(title, forKey: "title")
+    }
+    
+    required init(coder unarchiver: NSCoder) {
+        filePathURL = unarchiver.decodeObjectForKey("filePathURL") as! NSURL
+        title = unarchiver.decodeObjectForKey("title") as! String
     }
 }
